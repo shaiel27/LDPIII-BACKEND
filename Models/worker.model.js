39f5c7e84bf3a1153d0ac8ba8e6edec30d351746
Shaiel Becerra <shaielbecerra@gmail.com>
2024-12-08 12:13:46 -0400

    administracion de roles de los trabajadores

--- Models/worker.model.js
+++ Models/worker.model.js
@@ -1,42 +1,72 @@
 import { db } from '../database/connection.database.js'
 
-const create = async ({id, first_name, last_name, role_id, telephone_number, location, email, password, status, gender, emergency_contact_name, emergency_contact_relationship, emergency_contact_number, License_number, Years_experience, education, Certifications, date_birth }) => {
-    try {
-        const query = {
-            text: `
-            INSERT INTO "worker" ("id", "first_name", "last_name", "role_id", "telephone number", "location", "email", "password", "status", "gender", "emergency_contact_name", "emergency_contact_relationship", "emergency_contact_number", "License_number", "Years_experience", "education", "Certifications", "date_birth")
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)
-            RETURNING *
-            `,
-            values: [id, first_name, last_name, role_id, telephone_number, location, email, password, status, gender, emergency_contact_name, emergency_contact_relationship, emergency_contact_number, License_number, Years_experience, education, Certifications, date_birth]
-        }
-        const { rows } = await db.query(query)
-        return rows[0]
-    } catch (error) {
-        console.error('Error in create:', error);
-        throw error;
+const create = async ({ first_name, last_name, role_id, telephone_number, location, email, password, status, gender, emergency_contact_name, emergency_contact_relationship, emergency_contact_number, License_number, Years_experience, education, Certifications, date_birth }) => {
+    const query = {
+        text: `
+        INSERT INTO workers (first_name, last_name, role_id, telephone_number, location, email, password, status, gender, emergency_contact_name, emergency_contact_relationship, emergency_contact_number, License_number, Years_experience, education, Certifications, date_birth)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+        RETURNING email, first_name, last_name, uid, role_id
+        `,
+        values: [first_name, last_name, role_id, telephone_number, location, email, password, status, gender, emergency_contact_name, emergency_contact_relationship, emergency_contact_number, License_number, Years_experience, education, Certifications, date_birth]
     }
+
+    const { rows } = await db.query(query)
+    return rows[0]
 }
 
 const findOneByEmail = async (email) => {
-    try {
-        const query = {
-            text: `
-            SELECT * FROM "worker"
-            WHERE "email" = $1
-            `,
-            values: [email]
-        }
-        const { rows } = await db.query(query)
-        return rows[0]
-    } catch (error) {
-        console.error('Error en findOneByEmail:', error);
-        throw error;
+    const query = {
+        text: `
+        SELECT * FROM workers
+        WHERE email = $1
+        `,
+        values: [email]
     }
+    const { rows } = await db.query(query)
+    return rows[0]
+}
+
+const findAll = async () => {
+    const query = {
+        text: `
+        SELECT * FROM workers
+        `
+    }
+    const { rows } = await db.query(query)
+    return rows
+}
+
+const findOneByUid = async (uid) => {
+    const query = {
+        text: `
+        SELECT * FROM workers
+        WHERE uid = $1
+        `,
+        values: [uid]
+    }
+    const { rows } = await db.query(query)
+    return rows[0]
+}
+
+const updateRoleVet = async (uid) => {
+    const query = {
+        text: `
+        UPDATE workers
+        SET role_id = 2
+        WHERE uid = $1
+        RETURNING *
+        `,
+        values: [uid]
+    }
+    const { rows } = await db.query(query)
+    return rows[0]
 }
 
 export const workerModel = {
     create,
-    findOneByEmail
+    findOneByEmail,
+    findAll,
+    findOneByUid,
+    updateRoleVet
 }
 
--- routes/worker.route.js
+++ routes/worker.route.js
@@ -1,10 +1,18 @@
-import {Router} from "express"
-import { workerController } from "../controllers/worker.controller.js"
-import { verifytoken } from "../Middlewares/jwt.middleware.js"
+import { Router } from "express";
+import { workerController } from "../controllers/worker.controller.js";
+import { verifyAdmin, verifyToken } from "../Middlewares/jwt.middleware.js";
 
 const router = Router()
+
+// api/v1/workers
+
 router.post('/register', workerController.register)
-router.post('/login',workerController.login)
-router.get('/profile',verifytoken,workerController.profile)
+router.post('/login', workerController.login)
+router.get('/profile', verifyToken, workerController.profile)
+
+// Admin
+router.get('/', verifyToken, verifyAdmin, workerController.findAll)
+router.put('/update-role-vet/:uid', verifyToken, verifyAdmin, workerController.updateRoleVet)
 
 export default router;
+
