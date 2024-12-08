bcba55d8aa51468ab0192463688a843465beafd9
Shaiel Becerra <shaielbecerra@gmail.com>
2024-12-08 11:43:26 -0400

    subida de archivos

--- Models/worker.model.js
+++ Models/worker.model.js
@@ -4,7 +4,7 @@ const create = async ({id, first_name, last_name, role_id, telephone_number, loc
     try {
         const query = {
             text: `
-            INSERT INTO "worker" ("id", "first_name", "last_name", "role_id", "telephone_number", "location", "email", "password", "status", "gender", "emergency_contact_name", "emergency_contact_relationship", "emergency_contact_number", "License_number", "Years_experience", "education", "Certifications", "date_birth")
+            INSERT INTO "worker" ("id", "first_name", "last_name", "role_id", "telephone number", "location", "email", "password", "status", "gender", "emergency_contact_name", "emergency_contact_relationship", "emergency_contact_number", "License_number", "Years_experience", "education", "Certifications", "date_birth")
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)
             RETURNING *
             `,
